e6f7df70ba08e2c8c21653efe8d60b80ec8ed627
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-08-28 22:01:28 -0500

    Semana3

--- react-semana3/app-agenda/src/Pages/UserPage.js
+++ react-semana3/app-agenda/src/Pages/UserPage.js
@@ -19,38 +19,48 @@ export const UserPage = () => {
 
     const getUsers = async () => {
         //Await retornar una promesa
-        const resp = await usersApi.get('https://reqres.in/api/users');
-        setUsers(resp.data.data)
-    }
-    return (
-        <div className="mt-5">
-            <h1>Listado de Usuarios</h1>
-            <hr />
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>id</th>
-                        <th>Email</th>
-                        <th>Name</th>
-                        <th>Photo</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        users.map(user => (
-                            <tr key={user.email}>
-                                <td>{user.id}</td>
-                                <td>{user.email}</td>
-                                <td>{user.first_name}{user.last_name}</td>
-                                <td>
-                                    <img src={user.avatar} className="img-thumbail" style={{ width: 70 }} alt="" />
-                                </td>
-                            </tr>
-                        ))
-                    }
+        try {
+            const resp = await usersApi.get('https://reqres.in/api/users2');
+            setUsers(resp.data.data)
+            if (!resp.ok) {
+                throw Error(resp.statusText);
+            }
 
-                </tbody>
-            </table>
-        </div>
-    )
-}
+        } catch (error) {
+            console.log(error);
+        
+
+        }
+    }     
+        return (
+            <div className="mt-5">
+                <h1>Listado de Usuarios</h1>
+                <hr />
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>Email</th>
+                            <th>Name</th>
+                            <th>Photo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map(user => (
+                                <tr key={user.email}>
+                                    <td>{user.id}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.first_name}{user.last_name}</td>
+                                    <td>
+                                        <img src={user.avatar} className="img-thumbail" style={{ width: 70 }} alt="" />
+                                    </td>
+                                </tr>
+                            ))
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
